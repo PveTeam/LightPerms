bb5750f75482bb8b47c10515587cbc1b5fb1a5ed
zznty <94796179+zznty@users.noreply.github.com>
2023-11-10 17:34:03 +0700

    correct command permission check

--- LuckPerms.Torch/Extensions/CommandExtensions.cs
+++ LuckPerms.Torch/Extensions/CommandExtensions.cs
@@ -7,6 +7,6 @@ public static class CommandExtensions
 {
     public static string GetPermissionString(this Command command)
     {
-        return $"minecraft.command.{string.Join(".", command.Path.Select(b => b.ToLowerInvariant()))}";
+        return $"command.{string.Join(".", command.Path.Select(b => b.ToLowerInvariant()))}";
     }
 }
